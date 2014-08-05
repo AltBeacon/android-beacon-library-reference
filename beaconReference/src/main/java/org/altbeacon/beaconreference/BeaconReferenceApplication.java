94eaf8d6d4a3a104490ad14ed2a8f7877b8dda9d
David G. Young <david@radiusnetworks.com>
2014-08-05 13:30:27 -0400

    reference working with 2.0 library

---
+++ beaconReference/src/main/java/org/altbeacon/beaconreference/BeaconReferenceApplication.java
@@ -0,0 +1,100 @@
+package org.altbeacon.beaconreference;
+
+import android.app.Application;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+import org.altbeacon.beacon.startup.RegionBootstrap;
+import org.altbeacon.beacon.startup.BootstrapNotifier;
+
+/**
+ * Created by dyoung on 12/13/13.
+ */
+public class BeaconReferenceApplication extends Application implements BootstrapNotifier {
+    private static final String TAG = "AndroidProximityReferenceApplication";
+    private RegionBootstrap regionBootstrap;
+    private BackgroundPowerSaver backgroundPowerSaver;
+    private boolean haveDetectedBeaconsSinceBoot = false;
+
+
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "setting up background monitoring for beacons and power saving");
+        // wake up the app when a beacon is seen
+        Region region = new Region("backgroundRegion",
+                null, null, null);
+        regionBootstrap = new RegionBootstrap(this, region);
+        BeaconManager.debug = true;
+
+        // simply constructing this class and holding a reference to it in your custom Application
+        // class will automatically cause the BeaconLibrary to save battery whenever the application
+        // is not visible.  This reduces bluetooth power usage by about 60%
+        backgroundPowerSaver = new BackgroundPowerSaver(this);
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int arg0, Region arg1) {
+        // This method is not used in this example
+    }
+
+    @Override
+    public void didEnterRegion(Region arg0) {
+        // In this example, this class sends a notification to the user whenever a Beacon
+        // matching a Region (defined above) are first seen.
+        Log.d(TAG, "did enter region.");
+        if (!haveDetectedBeaconsSinceBoot) {
+            Log.d(TAG, "auto launching MainActivity");
+
+            // The very first time since boot that we detect an beacon, we launch the
+            // MainActivity
+            Intent intent = new Intent(this, MonitoringActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            // Important:  make sure to add android:launchMode="singleInstance" in the manifest
+            // to keep multiple copies of this activity from getting created if the user has
+            // already manually launched the app.
+            this.startActivity(intent);
+            haveDetectedBeaconsSinceBoot = true;
+        } else {
+            // If we have already seen beacons and launched the MainActivity before, we simply
+            // send a notification to the user on subsequent detections.
+            Log.d(TAG, "Sending notification.");
+            sendNotification();
+        }
+
+
+    }
+
+    @Override
+    public void didExitRegion(Region arg0) {
+        Log.d(TAG, "exited region");
+    }
+
+    private void sendNotification() {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setContentTitle("Beacon Reference Application")
+                        .setContentText("An beacon is nearby.")
+                        .setSmallIcon(R.drawable.ic_launcher);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntent(new Intent(this, MonitoringActivity.class));
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        builder.setContentIntent(resultPendingIntent);
+        NotificationManager notificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, builder.build());
+    }
+
+}
