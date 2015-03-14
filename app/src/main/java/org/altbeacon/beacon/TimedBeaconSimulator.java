1b26bf29286ba01ddb6b6e302d8c2555d12ef3bf
David G. Young <davidgyoung@gmail.com>
2015-03-14 07:54:11 -0400

    Fix comments about how to use TimedBeaconSimulator

--- app/src/main/java/org/altbeacon/beacon/TimedBeaconSimulator.java
+++ app/src/main/java/org/altbeacon/beacon/TimedBeaconSimulator.java
@@ -21,10 +21,11 @@ public class TimedBeaconSimulator implements org.altbeacon.beacon.simulator.Beac
 	 * You may simulate detection of beacons by creating a class like this in your project.
 	 * This is especially useful for when you are testing in an Emulator or on a device without BluetoothLE capability.
 	 * 
-	 * Uncomment lines 36, 37, and 139 - 142 of MonitoringActivity.java and 
-	 * set USE_SIMULATED_BEACONS = true to initialize the sample code in this class.
+	 * Uncomment the lines in BeaconReferenceApplication starting with:
+	 *     // If you wish to test beacon detection in the Android Emulator, you can use code like this:
+	 * Then set USE_SIMULATED_BEACONS = true to initialize the sample code in this class.
 	 * If using a bluetooth incapable test device (i.e. Emulator), you will want to comment
-	 * out the verifyBluetooth() call on line 32 of MonitoringActivity.java as well.
+	 * out the verifyBluetooth() in MonitoringActivity.java as well.
 	 * 
 	 * Any simulated beacons will automatically be ignored when building for production.
 	 */
