db91a28aafc6015c1a6db7c394c1b4a9fa931985
Rennie Petersen <rp@merlinia.com>
2015-06-10 02:13:41 +0200

    Cosmetic changes.
    A few tiny changes. If I remember right, some of this involves changing
    tab characters to blanks.

--- app/src/main/java/org/altbeacon/beaconreference/RangingActivity.java
+++ app/src/main/java/org/altbeacon/beaconreference/RangingActivity.java
@@ -24,37 +24,40 @@ public class RangingActivity extends Activity implements BeaconConsumer {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_ranging);
+        setContentView(R.layout.activity_ranging);
 
         beaconManager.bind(this);
     }
+
     @Override 
     protected void onDestroy() {
         super.onDestroy();
         beaconManager.unbind(this);
     }
+
     @Override 
     protected void onPause() {
-    	super.onPause();
-    	if (beaconManager.isBound(this)) beaconManager.setBackgroundMode(true);
+        super.onPause();
+        if (beaconManager.isBound(this)) beaconManager.setBackgroundMode(true);
     }
+
     @Override 
     protected void onResume() {
-    	super.onResume();
-    	if (beaconManager.isBound(this)) beaconManager.setBackgroundMode(false);
+        super.onResume();
+        if (beaconManager.isBound(this)) beaconManager.setBackgroundMode(false);
     }
 
     @Override
     public void onBeaconServiceConnect() {
         beaconManager.setRangeNotifier(new RangeNotifier() {
-        @Override 
-        public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-            if (beacons.size() > 0) {
-            	EditText editText = (EditText)RangingActivity.this
-						.findViewById(R.id.rangingText);
-                Beacon firstBeacon = beacons.iterator().next();
-                logToDisplay("The first beacon "+firstBeacon.toString()+" is about "+firstBeacon.getDistance()+" meters away.");            }
-        }
+           @Override
+           public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+              if (beacons.size() > 0) {
+                 //EditText editText = (EditText)RangingActivity.this.findViewById(R.id.rangingText);
+                 Beacon firstBeacon = beacons.iterator().next();
+                 logToDisplay("The first beacon " + firstBeacon.toString() + " is about " + firstBeacon.getDistance() + " meters away.");
+              }
+           }
 
         });
 
@@ -62,13 +65,13 @@ public class RangingActivity extends Activity implements BeaconConsumer {
             beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
         } catch (RemoteException e) {   }
     }
+
     private void logToDisplay(final String line) {
-    	runOnUiThread(new Runnable() {
-    	    public void run() {
-    	    	EditText editText = (EditText)RangingActivity.this
-    					.findViewById(R.id.rangingText);
-    	    	editText.append(line+"\n");            	
-    	    }
-    	});
+        runOnUiThread(new Runnable() {
+            public void run() {
+                EditText editText = (EditText)RangingActivity.this.findViewById(R.id.rangingText);
+                editText.append(line+"\n");
+            }
+        });
     }
 }
